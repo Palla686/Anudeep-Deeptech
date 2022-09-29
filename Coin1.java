fa90573df62c3ddc64fbf5033c08d5f796f3fd3d
pallavi Mane <109073274+Palla686@users.noreply.github.com>
2022-09-29 16:34:06 +0530

    Add files via upload

---
+++ Coin1.java
@@ -0,0 +1,24 @@
+class Coin1
+{
+    static long count(int coins[], int n, int sum)
+{
+        int table[] = new int[sum + 1];
+ 
+    table[0] = 1;
+     for (int i = 0; i < n; i++)
+        for (int j = coins[i]; j <= sum; j++)
+            table[j] += table[j - coins[i]];
+ 
+    return table[sum];
+}
+ 
+    public static void main(String args[])
+    {
+        int coins[] = { 2,5, 3,6};
+        int n = coins.length;
+        int sum = 10;
+        System.out.println(count(coins, n, sum));
+    }
+}
+ 
+
