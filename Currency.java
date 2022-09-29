9e343f26df780cf30f469731379a1f2685492154
pallavi Mane <109073274+Palla686@users.noreply.github.com>
2022-09-29 15:42:56 +0530

    Add files via upload

---
+++ Currency.java
@@ -0,0 +1,34 @@
+class Currency 
+{
+    static int count(int coins[], int n, int sum)
+    {        
+    	// If sum is 0 then there is 1 solution
+
+        if (sum == 0)
+            return 1;
+                            // If sum is less than 0 then no
+
+        if (sum < 0)
+            return 0;
+        //If
+        if (n <= 0)
+            return 0;
+
+        // count is sum of solutions (i)
+
+        return count(coins, n - 1, sum)+ count(coins, n, sum - coins[n - 1]);
+
+    }
+    // Driver code
+
+	public static void main(String[] args) {
+		 {
+		        int coins[] = {1,2,3};
+		        int n = coins.length;
+		 
+		        System.out.println(count(coins, n, 4));
+		    }
+		}
+	}
+
+
